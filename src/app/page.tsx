b7eef117db661785d0d3d1be00c4e930f4d654a8
Jonathan Tsai <jontsai@users.noreply.github.com>
2026-09-02 22:52:51 -0700

    site: add ascii avatar and refine readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
+const portrait = [
+  '⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀',
+  '⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀',
+  '⠀⠀⠀⠀⢰⣿⣿⠟⠋⠉⠉⠙⠻⣿⣿⡆⠀⠀⠀⠀',
+  '⠀⠀⠀⠀⣿⣿⠁⠀⢀⣀⣀⡀⠀⠈⣿⣿⠀⠀⠀⠀',
+  '⠀⠀⠀⢸⣿⡇⠀⣴⣿⣿⣿⣿⣦⠀⢸⣿⡇⠀⠀⠀',
+  '⠀⠀⠀⢸⣿⡇⠀⣿⠋⢀⡀⠙⣿⠀⢸⣿⡇⠀⠀⠀',
+  '⠀⠀⠀⠀⣿⣷⠀⠘⠷⠿⠿⠾⠃⠀⣾⣿⠀⠀⠀⠀',
+  '⠀⠀⠀⠀⠸⣿⣷⣄⠀⠐⠒⠀⣠⣾⣿⠇⠀⠀⠀⠀',
+  '⠀⠀⠀⠀⠀⠈⠻⣿⣿⣶⣶⣿⣿⠟⠁⠀⠀⠀⠀⠀',
+  '⠀⠀⠀⢀⣤⣤⣤⡈⠙⠛⠛⠋⢁⣤⣤⣤⡀⠀⠀⠀',
+  '⠀⠀⣰⣿⣿⣿⣿⣿⣦⣀⣀⣴⣿⣿⣿⣿⣿⣆⠀⠀',
+  '⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀',
+  '⢰⣿⣿⣿⣿⡿⠋⠉⠻⣿⣿⠟⠉⠙⢿⣿⣿⣿⣿⡆',
+  '⢸⣿⣿⣿⣿⡇⠀⢠⣤⠈⠁⣤⡄⠀⢸⣿⣿⣿⣿⡇',
+  '⠈⠻⢿⣿⣿⣿⣦⣈⠛⣀⣀⠛⣁⣴⣿⣿⣿⡿⠟⠁',
+].join('\n');
+
 export default function Home() {
   return (
     <main>
-      <p>
-        <strong>jonts.ai</strong> is the home of Jonathan Tsai&apos;s personal AI assistant /
-        doppelganger. For the human, see{' '}
-        <a href="https://jontsai.com">jontsai.com</a>.
-      </p>
+      <section className="card" aria-label="jonts.ai landing page">
+        <pre className="avatar" aria-hidden="true">
+          {portrait}
+        </pre>
+        <p>
+          <strong>jonts.ai</strong> is the home of Jonathan Tsai&apos;s personal AI assistant /
+          doppelganger. For the human, see{' '}
+          <a href="https://jontsai.com">jontsai.com</a>.
+        </p>
+      </section>
     </main>
   );
 }
